0ef588ae35ca23026febcbf86e5ddf0df645fba8
donettaaa <dsdodoh@st.ug.edu.gh>
2024-06-19 22:19:10 +0000

    App.js

--- Donetta_App/App.js
+++ Donetta_App/App.js
@@ -4,3 +4,34 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
 import LoginScreen from './screens/LoginScreen';
 import Homepage from './screens/Homepage';
+
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Regular": require("./assets/fonts/PoppinsRegular-B2Bw.otf"),
+    "Poppins-Medium": require("./assets/fonts/PoppinsMedium-1JPv.otf"),
+    "Poppins-SemiBold": require("./assets/fonts/PoppinsSemibold-8l8n.otf"),
+    "Poppins-Bold": require("./assets/fonts/PoppinsBold-GdJA.otf"),
+  });
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      {hideSplashScreen ? (
+        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Home" component={Homepage} />
+        </Stack.Navigator>
+      ) : null}
+    </NavigationContainer>
+  );
+};
+
+export default App; 
